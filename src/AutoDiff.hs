1171165d0be5634263c4df6bfae76e26ec164f23
Eli Reisman <elireisman@github.com>
2023-08-26 21:28:24 -0700

    formatting and comments

--- src/AutoDiff.hs
+++ src/AutoDiff.hs
@@ -12,36 +12,38 @@ data Tensor = Const Double
             deriving (Show)
 
 
--- traverse the Tensor tree and calculate the expression's scalar value
+-- traverse the Tensor tree and evaluate the expression with all variables
+-- bound to a scalar value, resulting in a scalar
 eval :: Tensor -> Double
 eval t = case t of
-  (Const d)         -> d
-  (Var _ t')         -> eval t'
-  (UnaryExpr uop t') -> case uop of
-                         Negate -> 0 - (eval t')
-                         Log    -> log (eval t')
-                         Exp    -> exp (eval t')
+  (Const d)            -> d
+  (Var _ t')           -> eval t'
+  (UnaryExpr uop t')   -> case uop of
+                            Negate -> 0 - (eval t')
+                            Log    -> log (eval t')
+                            Exp    -> exp (eval t')
   (BinExpr bop tl tr)  -> case bop of
-                         Pow  -> (eval tl) ** (eval tr)
-                         Mult -> (eval tl) * (eval tr)
-                         Div  -> (eval tl) / (eval tr)
-                         Add  -> (eval tl) + (eval tr)
-                         Sub  -> (eval tl) - (eval tr)
+                            Pow  -> (eval tl) ** (eval tr)
+                            Mult -> (eval tl) * (eval tr)
+                            Div  -> (eval tl) / (eval tr)
+                            Add  -> (eval tl) + (eval tr)
+                            Sub  -> (eval tl) - (eval tr)
 
 
--- traverse the Tensor tree and eval the partial derivative of expression's scalar value
+-- traverse the Tensor tree and differentiate the expression and evaluate
+-- with all variables bound to a scalar value, resulting in a scalar
 diff :: Tensor -> String -> Double
 diff t wrt = case (t, wrt) of
-  ((Const _), _)             -> 0
-  ((Var name t'), wrt')        -> if name == wrt' then 1 else diff t' wrt'
-  ((UnaryExpr uop t'), wrt')   -> case uop of
-                                  Negate -> 0 - (diff t' wrt')
-                                  Log    -> (1 / (eval t')) * (diff t' wrt')
-                                  Exp    -> (diff t' wrt') * exp (eval t')
+  ((Const _), _)              -> 0
+  ((Var name t'), wrt')       -> if name == wrt' then 1 else diff t' wrt'
+  ((UnaryExpr uop t'), wrt')  -> case uop of
+                                   Negate -> 0 - (diff t' wrt')
+                                   Log    -> (1 / (eval t')) * (diff t' wrt')
+                                   Exp    -> (diff t' wrt') * exp (eval t')
   ((BinExpr bop tl tr), wrt') -> case bop of
-                                  Pow  -> dtr * (diff tl wrt') ** (dtr-1) where dtr = diff tr wrt'
-                                  Mult -> ((diff tl wrt') * (eval tr)) + ((eval tl) * (diff tr wrt'))
-                                  Div  -> (((diff tl wrt') * (eval tr)) - ((eval tl) * (diff tr wrt'))) / ((eval tr) * (eval tr))
-                                  Add  -> (diff tl wrt') + (diff tr wrt')
-                                  Sub  -> (diff tl wrt') - (diff tr wrt')
+                                   Pow  -> dtr * (diff tl wrt') ** (dtr-1) where dtr = diff tr wrt'
+                                   Mult -> ((diff tl wrt') * (eval tr)) + ((eval tl) * (diff tr wrt'))
+                                   Div  -> (((diff tl wrt') * (eval tr)) - ((eval tl) * (diff tr wrt'))) / ((eval tr) * (eval tr))
+                                   Add  -> (diff tl wrt') + (diff tr wrt')
+                                   Sub  -> (diff tl wrt') - (diff tr wrt')
 
